32ca6a955cab806c350c402528a8cc665729fae5
Mathieu Roy <2234476@cegepmontpetit.ca>
2024-09-17 16:49:50 -0400

    FCT Avancement méthode
    element, premier, dernier, remplis

--- TableauFixe.h
+++ TableauFixe.h
@@ -6,27 +6,45 @@
 #define TABLEAUFIXE_H
 
 
-
-template <typename T> class TableauFixe {
+template<typename T>
+class TableauFixe {
 private:
     T *tabPtr;
     size_t size;
+
 public:
     TableauFixe();
-    TableauFixe( int pLongueur);
-    TableauFixe(TableauFixe& pTableauACopier);
-    TableauFixe(TableauFixe&& pTableauAMouvoir);
+
+    TableauFixe(int pLongueur);
+
+    TableauFixe(int pLongueur, T pArray[]);
+
+    TableauFixe(TableauFixe &pTableauACopier);
+
+    TableauFixe(TableauFixe &&pTableauAMouvoir);
+
     int taille();
+
     bool estVide() const;
-    T& operator[](int pIndex);
-    T& element(int pIndex);
+
+    T &operator[](int pIndex);
+
+    T &element(int pIndex);
+
     T premier();
+
     T dernier();
+
     void remplis(T pValeur);
-    void echange(TableauFixe& pTabAEchanger);
-    TableauFixe& fusion(TableauFixe& pTabAFusionner);
-    TableauFixe& sousEnsemble(int pIndex,int pNbElement);
-    TableauFixe& sousEnsemble(int pIndex);
+
+    void echange(TableauFixe &pTabAEchanger);
+
+    TableauFixe &fusion(TableauFixe &pTabAFusionner);
+
+    TableauFixe &sousEnsemble(int pIndex, int pNbElement);
+
+    TableauFixe &sousEnsemble(int pIndex);
+
     ~TableauFixe();
 };
 
@@ -34,6 +52,15 @@ template<typename T>
 TableauFixe<T>::TableauFixe(const int pLongueur) {
     size = pLongueur;
     tabPtr = new T[size];
+}
+
+template<typename T>
+TableauFixe<T>::TableauFixe(const int pLongueur, T pArray[]) {
+    size = pLongueur;
+    tabPtr = new T[size];
+    for (int i = 0; i < size; i++) {
+        *(tabPtr + i) = pArray[i];
+    }
 }
 
 template<typename T>
@@ -52,8 +79,37 @@ bool TableauFixe<T>::estVide() const {
 
 template<typename T>
 T &TableauFixe<T>::operator[](int pIndex) {
-    return (*tabPtr)[pIndex];
+    return *(tabPtr + pIndex);
 }
 
+template<typename T>
+T &TableauFixe<T>::element(int pIndex) {
+    if (pIndex < 0 || pIndex >= size) {
+        throw std::invalid_argument("L'index doit etre entre 0 et " + std::to_string(pIndex));
+    }
+    return *(tabPtr + pIndex);
+}
+
+template<typename T>
+T TableauFixe<T>::premier() {
+    return *tabPtr;
+}
+
+template<typename T>
+T TableauFixe<T>::dernier() {
+    return *(tabPtr + size - 1);
+}
+
+template<typename T>
+void TableauFixe<T>::remplis(T pValeur) {
+    for (int i = 0; i < size; i++) {
+        *(tabPtr + i) = pValeur;
+    }
+}
+
+template<typename T>
+TableauFixe<T>::~TableauFixe() {
+    delete[] tabPtr;
+}
 
 #endif //TABLEAUFIXE_H
--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 #include "TableauFixe.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TableauFixe<int> tab(2);
+    tab[0] = 8;
+    tab[1] = 2;
+    std::cout << std::to_string(tab.element(0)) + ", " + std::to_string(tab[1]) << std::endl;
+    std::cout << std::to_string(tab.taille()) << std::endl;
+    std::cout << std::to_string(tab.estVide()) << std::endl;
+
+    int arr[3] = {4, 5, 7};
+    TableauFixe<int> tab2(3, arr);
+    std::cout << std::to_string(tab2.element(0)) + ", " + std::to_string(tab2[1]) << std::endl;
+    std::cout << std::to_string(tab2.premier()) << std::endl;
+    std::cout << std::to_string(tab2.dernier()) << std::endl;
+
+    tab2.remplis(6);
+    std::cout << std::to_string(tab2[0]) + "," + std::to_string(tab2[1]) + "," + std::to_string(tab2[2]) << std::endl;
     return 0;
 }
