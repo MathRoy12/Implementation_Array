63de1ac4b4e7d839ac7341a06e188a2dd8c3b574
Mathieu Roy <2234476@cegepmontpetit.ca>
2024-09-19 11:57:04 -0400

    BUGFIX Part2

--- TableauFixe.h
+++ TableauFixe.h
@@ -26,7 +26,7 @@ public:
 
     TableauFixe &operator=(const TableauFixe &pTableauACopier);
 
-    TableauFixe &operator=(TableauFixe &&pTableauACopier);
+    TableauFixe &operator=(TableauFixe &&pTableauAMouvoir);
 
     bool operator==(TableauFixe &pTableauACopier);
 
@@ -42,19 +42,19 @@ public:
 
     TableauFixe &fusion(TableauFixe &pTabAFusionner);
 
-    template<size_t Size>
-    TableauFixe sousEnsemble(int pIndex);
+    template<size_t NewSize>
+    TableauFixe<T, NewSize> sousEnsemble(int pIndex);
 
     ~TableauFixe();
 };
 
 template<typename T, size_t S>
-TableauFixe<T,S>::TableauFixe(){
+TableauFixe<T, S>::TableauFixe() {
     tabPtr = new T[S];
 }
 
 template<typename T, size_t S>
-TableauFixe<T,S>::TableauFixe(const TableauFixe &pTableauACopier) {
+TableauFixe<T, S>::TableauFixe(const TableauFixe &pTableauACopier) {
     tabPtr = new T[taille()];
 
     for (int i = 0; i < taille(); i++) {
@@ -63,28 +63,28 @@ TableauFixe<T,S>::TableauFixe(const TableauFixe &pTableauACopier) {
 }
 
 template<typename T, size_t S>
-TableauFixe<T,S>::TableauFixe(TableauFixe &&pTableauAMouvoir) {
+TableauFixe<T, S>::TableauFixe(TableauFixe &&pTableauAMouvoir) {
     tabPtr = pTableauAMouvoir.tabPtr;
     pTableauAMouvoir.tabPtr = nullptr;
 }
 
 template<typename T, size_t S>
-int TableauFixe<T,S>::taille() const {
+int TableauFixe<T, S>::taille() const {
     return S;
 }
 
 template<typename T, size_t S>
-bool TableauFixe<T,S>::estVide() const {
+bool TableauFixe<T, S>::estVide() const {
     return S == 0;
 }
 
 template<typename T, size_t S>
-T &TableauFixe<T,S>::operator[](size_t pIndex) const {
+T &TableauFixe<T, S>::operator[](size_t pIndex) const {
     return tabPtr[pIndex];
 }
 
 template<typename T, size_t S>
-TableauFixe<T,S> &TableauFixe<T,S>::operator=(const TableauFixe &pTableauACopier) {
+TableauFixe<T, S> &TableauFixe<T, S>::operator=(const TableauFixe &pTableauACopier) {
     if (&pTableauACopier == this) {
         return *this;
     }
@@ -99,7 +99,7 @@ TableauFixe<T,S> &TableauFixe<T,S>::operator=(const TableauFixe &pTableauACopier
 }
 
 template<typename T, size_t S>
-TableauFixe<T,S> &TableauFixe<T,S>::operator=(TableauFixe &&pTableauAMouvoir) {
+TableauFixe<T, S> &TableauFixe<T, S>::operator=(TableauFixe &&pTableauAMouvoir) {
     if (&pTableauAMouvoir == this) {
         return *this;
     }
@@ -111,7 +111,7 @@ TableauFixe<T,S> &TableauFixe<T,S>::operator=(TableauFixe &&pTableauAMouvoir) {
 }
 
 template<typename T, size_t S>
-T &TableauFixe<T,S>::element(size_t pIndex) const {
+T &TableauFixe<T, S>::element(size_t pIndex) const {
     if (pIndex >= taille()) {
         throw std::invalid_argument("L'index doit etre inférieure à " + std::to_string(taille()));
     }
@@ -119,34 +119,34 @@ T &TableauFixe<T,S>::element(size_t pIndex) const {
 }
 
 template<typename T, size_t S>
-T &TableauFixe<T,S>::premier() const {
+T &TableauFixe<T, S>::premier() const {
     return *tabPtr;
 }
 
 template<typename T, size_t S>
-T &TableauFixe<T,S>::dernier() const {
+T &TableauFixe<T, S>::dernier() const {
     return tabPtr[taille() - 1];
 }
 
 template<typename T, size_t S>
-void TableauFixe<T,S>::remplis(T pValeur) {
+void TableauFixe<T, S>::remplis(T pValeur) {
     for (int i = 0; i < taille(); i++) {
         tabPtr[i] = pValeur;
     }
 }
 
 template<typename T, size_t S>
-template<size_t Size>
-TableauFixe<T,S> TableauFixe<T,S>::sousEnsemble(int pIndex) {
-    TableauFixe<T,Size> arr();
-    for (int i = 0; i < Size; i++) {
+template<size_t NewSize>
+TableauFixe<T, NewSize> TableauFixe<T, S>::sousEnsemble(int pIndex) {
+    TableauFixe<T, NewSize> arr;
+    for (int i = 0; i < NewSize; i++) {
         arr[i] = tabPtr[pIndex + i];
     }
     return arr;
 }
 
 template<typename T, size_t S>
-TableauFixe<T,S>::~TableauFixe() {
+TableauFixe<T, S>::~TableauFixe() {
     delete[] tabPtr;
 }
 
--- main.cpp
+++ main.cpp
@@ -12,12 +12,15 @@ int main() {
     std::cout << std::to_string(tab.estVide()) << std::endl;
 
     TableauFixe<int,3> tab2;
-    tab[0] = 3;
-    tab[1] = 8;
-    tab[2] = 5;
-    std::cout << std::to_string(tab[0]) + "," + std::to_string(tab[1]) + "," + std::to_string(tab[2])<< std::endl;
-    std::cout << std::to_string(tab.premier()) << std::endl;
-    std::cout << std::to_string(tab.dernier()) << std::endl;
+    tab2[0] = 3;
+    tab2[1] = 8;
+    tab2[2] = 5;
+    std::cout << std::to_string(tab2[0]) + "," + std::to_string(tab2[1]) + "," + std::to_string(tab2[2])<< std::endl;
+    std::cout << std::to_string(tab2.premier()) << std::endl;
+    std::cout << std::to_string(tab2.dernier()) << std::endl;
+
+    tab = tab2.sousEnsemble<2>(1);
+    std::cout << std::to_string(tab[0]) + "," + std::to_string(tab[1]) << std::endl;
 
     return 0;
 }
